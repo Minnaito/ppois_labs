c34f47865ce79e7503fde36e91af30720b2bfc4b
Minna <mariagorbunova2007@gmail.com>
2025-11-22 08:48:40 +0300

    Create constants.py

---
+++ laba1/polynomial_project/src/constants.py
@@ -0,0 +1,14 @@
+class constants:
+    DEFAULT_COEFFICIENT = 0
+    DEFAULT_COEFFICIENTS = [0]
+
+    ZERO_THRESHOLD = 1e-10
+    PRECISION = 6
+
+    START_INDEX = 0
+    MIN_LENGTH = 1
+    SINGLE_ELEMENT = 1
+
+    ERROR_DIVISION_BY_ZERO = "Деление на ноль"
+    ERROR_UNSUPPORTED_TYPE = "Неподдерживаемый тип для операции"
+    ERROR_INVALID_COEFFICIENTS = "Коэффициенты должны быть списком, int или float"
