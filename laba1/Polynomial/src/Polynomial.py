35e7f6e0ddf67c177d99e110aecc4afd937be7f6
Minna <mariagorbunova2007@gmail.com>
2025-11-26 19:41:59 +0300

    Update Polynomial.py

--- laba1/Polynomial/src/Polynomial.py
+++ laba1/Polynomial/src/Polynomial.py
@@ -5,108 +5,108 @@ from .Constants import Constants
 class Polynomial(PolynomialBase):
 
     def __add__(self, other):
-        constantsObj = Constants()
+        constants_obj = Constants()
 
         if isinstance(other, (int, float)):
             other = Polynomial([other])
 
-        maxDegreeValue = max(self.degree, other.degree)
-        resultCoefficients = [float(constantsObj.DEFAULT_COEFFICIENT)] * (maxDegreeValue + constantsObj.MIN_LENGTH)
+        max_degree_value = max(self.degree, other.degree)
+        result_coefficients = [float(constants_obj.DEFAULT_COEFFICIENT)] * (max_degree_value + constants_obj.MIN_LENGTH)
 
-        for currentIndex in range(maxDegreeValue + constantsObj.MIN_LENGTH):
-            coefficientSelf = self[self.degree - currentIndex] if currentIndex <= self.degree else float(
-                constantsObj.DEFAULT_COEFFICIENT)
-            coefficientOther = other[other.degree - currentIndex] if currentIndex <= other.degree else float(
-                constantsObj.DEFAULT_COEFFICIENT)
-            resultCoefficients[currentIndex] = coefficientSelf + coefficientOther
+        for current_index in range(max_degree_value + constants_obj.MIN_LENGTH):
+            coefficient_self = self[self.degree - current_index] if current_index <= self.degree else float(
+                constants_obj.DEFAULT_COEFFICIENT)
+            coefficient_other = other[other.degree - current_index] if current_index <= other.degree else float(
+                constants_obj.DEFAULT_COEFFICIENT)
+            result_coefficients[current_index] = coefficient_self + coefficient_other
 
-        return Polynomial(resultCoefficients[::-constantsObj.MIN_LENGTH])
+        return Polynomial(result_coefficients[::-constants_obj.MIN_LENGTH])
 
     def __iadd__(self, other):
-        resultPolynomial = self + other
-        self._coefficients = resultPolynomial._coefficients
-        self._degree = resultPolynomial._degree
+        result_polynomial = self + other
+        self._coefficients = result_polynomial._coefficients
+        self._degree = result_polynomial._degree
         return self
 
     def __sub__(self, other):
-        constantsObj = Constants()
+        constants_obj = Constants()
 
         if isinstance(other, (int, float)):
             other = Polynomial([other])
 
-        maxDegreeValue = max(self.degree, other.degree)
-        resultCoefficients = [float(constantsObj.DEFAULT_COEFFICIENT)] * (maxDegreeValue + constantsObj.MIN_LENGTH)
+        max_degree_value = max(self.degree, other.degree)
+        result_coefficients = [float(constants_obj.DEFAULT_COEFFICIENT)] * (max_degree_value + constants_obj.MIN_LENGTH)
 
-        for currentIndex in range(maxDegreeValue + constantsObj.MIN_LENGTH):
-            coefficientSelf = self[self.degree - currentIndex] if currentIndex <= self.degree else float(
-                constantsObj.DEFAULT_COEFFICIENT)
-            coefficientOther = other[other.degree - currentIndex] if currentIndex <= other.degree else float(
-                constantsObj.DEFAULT_COEFFICIENT)
-            resultCoefficients[currentIndex] = coefficientSelf - coefficientOther
+        for current_index in range(max_degree_value + constants_obj.MIN_LENGTH):
+            coefficient_self = self[self.degree - current_index] if current_index <= self.degree else float(
+                constants_obj.DEFAULT_COEFFICIENT)
+            coefficient_other = other[other.degree - current_index] if current_index <= other.degree else float(
+                constants_obj.DEFAULT_COEFFICIENT)
+            result_coefficients[current_index] = coefficient_self - coefficient_other
 
-        return Polynomial(resultCoefficients[::-constantsObj.MIN_LENGTH])
+        return Polynomial(result_coefficients[::-constants_obj.MIN_LENGTH])
 
     def __isub__(self, other):
-        resultPolynomial = self - other
-        self._coefficients = resultPolynomial._coefficients
-        self._degree = resultPolynomial._degree
+        result_polynomial = self - other
+        self._coefficients = result_polynomial._coefficients
+        self._degree = result_polynomial._degree
         return self
 
     def __mul__(self, other):
-        constantsObj = Constants()
+        constants_obj = Constants()
 
         if isinstance(other, (int, float)):
-            resultCoefficients = [coefficient * other for coefficient in self._coefficients]
-            return Polynomial(resultCoefficients)
+            result_coefficients = [coefficient * other for coefficient in self._coefficients]
+            return Polynomial(result_coefficients)
 
-        resultDegreeValue = self._degree + other._degree
-        resultCoefficients = [float(constantsObj.DEFAULT_COEFFICIENT)] * (
-                    resultDegreeValue + constantsObj.MIN_LENGTH)
+        result_degree_value = self._degree + other._degree
+        result_coefficients = [float(constants_obj.DEFAULT_COEFFICIENT)] * (
+                    result_degree_value + constants_obj.MIN_LENGTH)
 
-        for outerIndex in range(self._degree + constantsObj.MIN_LENGTH):
-            for innerIndex in range(other._degree + constantsObj.MIN_LENGTH):
-                resultCoefficients[outerIndex + innerIndex] += self._coefficients[outerIndex] * other._coefficients[
-                    innerIndex]
+        for outer_index in range(self._degree + constants_obj.MIN_LENGTH):
+            for inner_index in range(other._degree + constants_obj.MIN_LENGTH):
+                result_coefficients[outer_index + inner_index] += self._coefficients[outer_index] * other._coefficients[
+                    inner_index]
 
-        return Polynomial(resultCoefficients)
+        return Polynomial(result_coefficients)
 
     def __imul__(self, other):
-        resultPolynomial = self * other
-        self._coefficients = resultPolynomial._coefficients
-        self._degree = resultPolynomial._degree
+        result_polynomial = self * other
+        self._coefficients = result_polynomial._coefficients
+        self._degree = result_polynomial._degree
         return self
 
     def __truediv__(self, other):
-        constantsObj = Constants()
+        constants_obj = Constants()
 
         if isinstance(other, (int, float)):
-            if abs(other) < constantsObj.ZERO_THRESHOLD:
-                raise ZeroDivisionError(constantsObj.ERROR_DIVISION_BY_ZERO)
-            resultCoefficients = [coefficient / other for coefficient in self._coefficients]
-            return Polynomial(resultCoefficients)
+            if abs(other) < constants_obj.ZERO_THRESHOLD:
+                raise ZeroDivisionError(constants_obj.ERROR_DIVISION_BY_ZERO)
+            result_coefficients = [coefficient / other for coefficient in self._coefficients]
+            return Polynomial(result_coefficients)
 
         if other._degree > self._degree:
-            return Polynomial([constantsObj.DEFAULT_COEFFICIENT])
+            return Polynomial([constants_obj.DEFAULT_COEFFICIENT])
 
-        dividendCoefficients = self._coefficients.copy()
-        divisorCoefficients = other._coefficients
-        quotientDegreeValue = self._degree - other._degree
-        quotientCoefficients = [float(constantsObj.DEFAULT_COEFFICIENT)] * (
-                    quotientDegreeValue + constantsObj.MIN_LENGTH)
+        dividend_coefficients = self._coefficients.copy()
+        divisor_coefficients = other._coefficients
+        quotient_degree_value = self._degree - other._degree
+        quotient_coefficients = [float(constants_obj.DEFAULT_COEFFICIENT)] * (
+                    quotient_degree_value + constants_obj.MIN_LENGTH)
 
-        for divisionIndex in range(quotientDegreeValue + constantsObj.MIN_LENGTH):
-            quotientCoefficient = dividendCoefficients[divisionIndex] / divisorCoefficients[
-                constantsObj.START_INDEX]
-            quotientCoefficients[divisionIndex] = quotientCoefficient
+        for division_index in range(quotient_degree_value + constants_obj.MIN_LENGTH):
+            quotient_coefficient = dividend_coefficients[division_index] / divisor_coefficients[
+                constants_obj.START_INDEX]
+            quotient_coefficients[division_index] = quotient_coefficient
 
-            for subtractionIndex in range(len(divisorCoefficients)):
-                dividendCoefficients[divisionIndex + subtractionIndex] -= quotientCoefficient * \
-                                                                             divisorCoefficients[subtractionIndex]
+            for subtraction_index in range(len(divisor_coefficients)):
+                dividend_coefficients[division_index + subtraction_index] -= quotient_coefficient * \
+                                                                             divisor_coefficients[subtraction_index]
 
-        return Polynomial(quotientCoefficients)
+        return Polynomial(quotient_coefficients)
 
     def __itruediv__(self, other):
-        resultPolynomial = self / other
-        self._coefficients = resultPolynomial._coefficients
-        self._degree = resultPolynomial._degree
+        result_polynomial = self / other
+        self._coefficients = result_polynomial._coefficients
+        self._degree = result_polynomial._degree
         return self
