7a7b1d7d97b02cd9bbc48706539597bcfefa0af6
Minna <mariagorbunova2007@gmail.com>
2025-12-06 07:20:50 +0300

    Update SuspensionSystem.py

--- lab2/CarFactory/models/production/SuspensionSystem.py
+++ lab2/CarFactory/models/production/SuspensionSystem.py
@@ -1,16 +1,17 @@
 from models.production.CarPart import CarPart
+from config import constants
 
 class SuspensionSystem(CarPart):
     def __init__(self, partIdentifier: str, partName: str, materialType: str,
                  partWeight: float, suspensionType: str):
-        super().__init__(partIdentifier, partName, materialType, partWeight)  # ← 4 параметра!
+        super().__init__(partIdentifier, partName, materialType, partWeight, "SuspensionSystem")
         self._suspensionType = suspensionType
 
     def calculateProductionCost(self) -> float:
         baseCost = super().calculate_cost()
-        typeCost = len(self._suspensionType) * 30
+        typeCost = len(self._suspensionType) * constants.SUSPENSION_TYPE_COST_FACTOR
         return baseCost + typeCost
 
     def performQualityCheck(self) -> bool:
         super().check_quality()
-        return self._suspensionType in ["независимая", "зависимая", "independent", "dependent"]
+        return self._suspensionType in constants.VALID_SUSPENSION_TYPES
