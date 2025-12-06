eb521cc1dacbd6a06787aa1dfef2900221b8d2f8
Minna <mariagorbunova2007@gmail.com>
2025-12-06 07:17:05 +0300

    Update ElectricalSystem.py

--- lab2/CarFactory/models/production/ElectricalSystem.py
+++ lab2/CarFactory/models/production/ElectricalSystem.py
@@ -4,19 +4,24 @@ from config import constants
 class ElectricalSystem(CarPart):
     def __init__(self, partIdentifier: str, partName: str, materialType: str,
                  partWeight: float, systemVoltage: float):
-        super().__init__(partIdentifier, partName, materialType, partWeight)  # ← 4 параметра!
+        # CarPart ожидает 5 параметров, добавляем тип системы
+        super().__init__(partIdentifier, partName, materialType, partWeight, "ElectricalSystem")
         self._systemVoltage = systemVoltage
 
     def calculateProductionCost(self) -> float:
         baseCost = super().calculate_cost()
-        voltageCost = self._systemVoltage * 2
+        voltageCost = self._systemVoltage * constants.ELECTRICAL_VOLTAGE_COST_FACTOR
         return baseCost + voltageCost
 
     def performQualityCheck(self) -> bool:
         super().check_quality()
-        return 12 <= self._systemVoltage <= 48
+        return (constants.MIN_ELECTRICAL_VOLTAGE <= self._systemVoltage <= 
+                constants.MAX_ELECTRICAL_VOLTAGE)
 
     def getElectricalSpecifications(self) -> dict:
         return {
-            "systemVoltage": self._systemVoltage
+            "systemVoltage": self._systemVoltage,
+            "min_allowed_voltage": constants.MIN_ELECTRICAL_VOLTAGE,
+            "max_allowed_voltage": constants.MAX_ELECTRICAL_VOLTAGE,
+            "voltage_cost_factor": constants.ELECTRICAL_VOLTAGE_COST_FACTOR
         }
