3008e0653da163d2a4139c9d6be9b8fdadafb5ba
Minna <mariagorbunova2007@gmail.com>
2025-12-06 07:00:37 +0300

    Update Invoice.py

--- lab2/CarFactory/models/finance/Invoice.py
+++ lab2/CarFactory/models/finance/Invoice.py
@@ -4,10 +4,10 @@ class Invoice:
     def __init__(self, invoice_id: str, amount: float):
         self._invoice_id = invoice_id
         self._amount = amount
-        self._status = constants.TRANSACTION_STATUSES[constants.ZERO_VALUE]  # "PENDING"
+        self._status = constants.TRANSACTION_STATUSES[constants.ZERO_VALUE]  
 
     def process(self) -> dict:
-        self._status = constants.TRANSACTION_STATUSES[constants.ZERO_VALUE + 1]  # "APPROVED"
+        self._status = constants.TRANSACTION_STATUSES[constants.ZERO_VALUE + 1]  
         tax = self._amount * constants.STANDARD_TAX_RATE
         return {
             "invoice_id": self._invoice_id,
@@ -15,4 +15,5 @@ class Invoice:
             "tax": tax,
             "total": self._amount + tax,
             "status": self._status
+
         }
