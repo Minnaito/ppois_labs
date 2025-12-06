f0cb8ba59b19164595f146cc601f70f5e8f4d705
Minna <mariagorbunova2007@gmail.com>
2025-12-06 07:03:34 +0300

    Update MachineMaintenance.py

--- lab2/CarFactory/models/maintenance/MachineMaintenance.py
+++ lab2/CarFactory/models/maintenance/MachineMaintenance.py
@@ -4,13 +4,14 @@ class MachineMaintenance:
     def __init__(self, maint_id: str, machine_id: str):
         self._maint_id = maint_id
         self._machine_id = machine_id
-        self._status = constants.PRODUCTION_ORDER_STATUSES[constants.ZERO_VALUE]  # "PENDING"
+        self._status = constants.PRODUCTION_ORDER_STATUSES[constants.ZERO_VALUE]  
 
     def start(self) -> None:
-        self._status = constants.PRODUCTION_ORDER_STATUSES[constants.ZERO_VALUE + 1]  # "IN_PROGRESS"
+        self._status = constants.PRODUCTION_ORDER_STATUSES[constants.ZERO_VALUE + 1]  
 
     def complete(self) -> None:
-        self._status = constants.PRODUCTION_ORDER_STATUSES[constants.ZERO_VALUE + 2]  # "COMPLETED"
+        self._status = constants.PRODUCTION_ORDER_STATUSES[constants.ZERO_VALUE + 2]  
 
     def get_report(self) -> dict:
+
         return {"id": self._maint_id, "machine": self._machine_id, "status": self._status}
