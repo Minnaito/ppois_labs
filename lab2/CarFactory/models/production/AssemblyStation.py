d8c5f8c88c5096386a6441ba8217e45f87c7b01c
Minna <mariagorbunova2007@gmail.com>
2025-12-06 07:07:40 +0300

    Update AssemblyStation.py

--- lab2/CarFactory/models/production/AssemblyStation.py
+++ lab2/CarFactory/models/production/AssemblyStation.py
@@ -1,16 +1,19 @@
+from config import constants
+
 class AssemblyStation:
     """Упрощенная сборочная станция"""
 
     def __init__(self, stationIdentifier: str, stationType: str):
         self._stationIdentifier = stationIdentifier
         self._stationType = stationType
-        self._partsCount = 0  # 3 поля вместо 8
+        self._partsCount = constants.ZERO_VALUE
 
     def addPart(self):
         """Добавить деталь"""
-        self._partsCount += 1
+        self._partsCount += constants.ZERO_VALUE + 1
         return True
 
     def assemble(self):
         """Собрать детали"""
-        return self._partsCount > 0
+
+        return self._partsCount > constants.ZERO_VALUE
